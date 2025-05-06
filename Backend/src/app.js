3b00c77bb6cb63a989ae1532a9501cbe6b711f1c
ashnik-nova <ashnik2511@gmail.com>
2025-05-06 18:48:06 +0530

    Backend cor link changed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://sant-tukaram-maharaj-literature.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
